ab66087567ab2541c6d8c1c104368cb6e2e4deb3
Trey Crossley <treycrossley@gmail.com>
2024-01-20 03:19:07 -0800

    middleware error

--- app.ts
+++ app.ts
@@ -2,17 +2,18 @@ import { ApolloServer } from 'apollo-server-express'
 import dotenv from 'dotenv'
 import { typeDefs, resolvers } from './graphql'
 import express, { Express, Request, Response } from 'express'
-import { sendEmail } from './services/email-service'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
+app.use(cors())
 const server = new ApolloServer({
     typeDefs,
     resolvers,
 })
 await server.start()
-server.applyMiddleware({ app })
+server.applyMiddleware({app})
 const PORT = Number(process.env.PORT ?? 4000)
 
 app.get('/', (req: Request, res: Response) => {
